1e1dfd8938ea026a07b9abb9f30b5c2edf1cf880
notquitethereyet <arnav.panigrahi@gmail.com>
2024-11-28 02:48:27 -0800

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/notquitethereyet/' // Matches your repository name
+  base: '/cutesite/' // Matches your repository name
 });
